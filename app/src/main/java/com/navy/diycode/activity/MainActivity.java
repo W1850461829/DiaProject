4f4251249e9f4f1b6479c23b4347f15a12febbd0
w1850461829 <1850461829@qq.com>
2017-11-17 12:18:32 +0800

    第一次上传

---
+++ app/src/main/java/com/navy/diycode/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.navy.diycode.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.navy.diycode.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
